13ec18374b7c96b156d9e27d51c6619fca0af187
ThePrimeagen <the.primeagen@aol.com>
2025-10-24 20:03:51 +0200

    Added Input and Output folders. Modified script to wirk with them

--- README.md
+++ README.md
@@ -8,7 +8,10 @@ Python script for merging pdf files
 
 ## Usage ##
 
+First, place the files you want to merge into the Input directory.
+
 ```bash
 python3 pdf_fusion.py <output_name.pdf> <input1.pdf> <input2.pdf> ... <inputN.pdf>
 ```
 The first argument is the name that will be given to the produced pdf. All the following arguments are the pdf files to merge, appearing in the order of the inputs.
+The resulting pdf will appear in the Output directory.
--- pdf_fusion.py
+++ pdf_fusion.py
@@ -4,12 +4,14 @@ import pypdf
 
 
 output_name = sys.argv[1]
-
+input_pdfs = []
+for pdf in sys.argv[2:]:
+    input_pdfs.append(os.path.join("Input",pdf))
 if ".pdf" not in output_name[-4:]:
     output_name += ".pdf"
 
 i = 1
-new_output_name = output_name
+new_output_name = os.path.join("Output",output_name)
 while os.path.exists(new_output_name):
     new_output_name = output_name[:-4] + f"({i})" + ".pdf"
     i+=1
@@ -19,7 +21,7 @@ output_name = new_output_name
 
 merger = pypdf.PdfWriter()
 
-for pdf in sys.argv[2:]:
+for pdf in input_pdfs:
     merger.append(pdf)
 
 merger.write(output_name)
